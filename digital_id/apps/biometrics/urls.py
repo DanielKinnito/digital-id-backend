d213285e3ac8b431dda6599de03054afe8d4ace1
DanielKinnito <mokonnendaniel@gmail.com>
2025-01-21 01:15:02 +0300

    :bug: Fix import errors in biometrics URLs

--- digital_id/apps/biometrics/urls.py
+++ digital_id/apps/biometrics/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserViewSet
+from .views import UserBiometricViewSet
 
 urlpatterns = [
-    path('register/', UserViewSet.as_view({'post': 'create'}), name='user-register'),
-    path('profile/', UserViewSet.as_view({'get': 'retrieve'}), name='user-profile'),
-    path('update/', UserViewSet.as_view({'put': 'update'}), name='user-update'),
-    path('biometrics/', UserViewSet.as_view({'get': 'retrieve'}), name='user-biometrics'),
+    path('register/', UserBiometricViewSet.as_view({'post': 'create'}), name='user-register'),
+    path('profile/', UserBiometricViewSet.as_view({'get': 'retrieve'}), name='user-profile'),
+    path('update/', UserBiometricViewSet.as_view({'put': 'update'}), name='user-update'),
+    path('biometrics/', UserBiometricViewSet.as_view({'get': 'retrieve'}), name='user-biometrics'),
 ]
