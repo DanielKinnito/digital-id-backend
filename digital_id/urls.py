7e9615147bb9d7f64d5ad919a8b61da73ec1d7ad
DanielKinnito <mokonnendaniel@gmail.com>
2025-01-21 00:10:10 +0300

    :passport_control: Update urls.py to enable rest_framework urls.

--- digital_id/urls.py
+++ digital_id/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('users/', include('digital_id.apps.users.urls')),
     path('ids/', include('digital_id.apps.ids.urls')),
     path('biometrics/', include('digital_id.apps.biometrics.urls')),
